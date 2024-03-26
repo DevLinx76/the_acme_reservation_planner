278fc0591134dafec89a9b37c8a2e18cc7be95af
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2024-03-26 18:36:25 -0400

    edited

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_reservation_planner_db');
 const uuid = require('uuid');
-const express = require('express');
+
 
 // Create tables
 const createTables = async()=> {
